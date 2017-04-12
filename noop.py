f88adb048ab784ce9605f5e659a00127f6f41db0
Lucas Natraj <lnatraj@slb.com>
2017-04-12 15:34:32 -0700

    change it to return an unauthorized

--- noop.py
+++ noop.py
@@ -4,7 +4,8 @@ import webapp2
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('yo!')
+        self.response.write('unauthorized!')
+        self.response.set_status(401)
 
 
 application = webapp2.WSGIApplication([
